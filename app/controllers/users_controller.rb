da422af335e20af2217f536ff9645437cd8520be
Sacha <sacha@hellojam.fr>
2015-08-19 17:13:41 +0200

    Correction style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    #debugger
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # Handle a successful save.
     else
       render 'new'
     end
