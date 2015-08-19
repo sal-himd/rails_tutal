cf4a6f5728b5962f6238da5ee917d47b73d63b40
Sacha <sacha@hellojam.fr>
2015-08-19 16:32:45 +0200

    Mod : use sample instead of rand

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-5.times do |n|
-  name = "name#{n + 1}"
-  email = "example#{n + 1}@exemple.com"
+
+users = Array.new
+5.times do |indice|
+  name = "name#{indice + 1}"
+  email = "example#{indice + 1}@exemple.com"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  users << User.create!(name: name, email: email, password: password, password_confirmation: password)
 end
 
-5.times do |n|
-  title = "title#{n + 1}"
+5.times do |indice|
+  title = "title#{indice + 1}"
   content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  user_id = 1 + Random.new.rand(5)
+  user_id = users.sample.id
   Post.create!(title: title, content: content, user_id: user_id)
 end
