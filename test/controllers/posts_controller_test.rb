ab5f1cbae9273e995160fb8275af9ec0a666ba45
Sacha <sacha@hellojam.fr>
2015-08-19 17:22:54 +0200

    Rm test new, Add test index

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+  test "should get index" do
+    get :index
     assert_response :success
   end
 
